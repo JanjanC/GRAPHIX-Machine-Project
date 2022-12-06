0d72e494bc039473b5218205d5972639a43cac43
iDLEWINK <josh_uson@dlsu.edu.ph>
2022-12-06 17:06:32 +0800

    refactor: OOP camera headers

---
+++ Classes/Cameras/MyCamera.h
@@ -0,0 +1,42 @@
+#include "../Models/Shader.h"
+#pragma once
+
+class MyCamera {
+
+public:
+    glm::vec3 position; //position of the camera in the world
+    glm::vec3 target; //position of the target in the world
+    glm::vec3 up; //the world up vector
+    float distance; //distance from the camera to the target
+
+    //constructor for the camera class
+    MyCamera(glm::vec3 position, glm::vec3 target, glm::vec3 up) {
+        this->position = position;
+        this->target = target;
+        this->up = up;
+        distance = glm::length(target - position); //computes for the distance from the camera to its target
+    }
+
+    //set the value of the view matrix in the shader
+    void setViewMatrix(Shader shader) {
+        shader.useProgram();
+
+        //computes for the view matrix
+        glm::mat4 viewMatrix = glm::lookAt(position, target, up);
+
+        //set the value of the view matrix in the shader
+        unsigned int viewLoc = glGetUniformLocation(shader.shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+    }
+    //set the value of the cameraPos in the shader
+    void setCameraPosition(Shader shader) {
+        shader.useProgram();
+
+        //set the value of the cameraPos in the shader
+        unsigned int cameraPosLoc = glGetUniformLocation(shader.shaderProgram, "cameraPos");
+        glUniform3fv(cameraPosLoc, 1, glm::value_ptr(position));
+    }
+
+    //pure virtual function that sets the value of the projection matrix in the shader
+    virtual void setProjectionMatrix(Shader shader) = 0;
+};
---
+++ Classes/Cameras/PerspectiveCamera.h
@@ -0,0 +1,81 @@
+#include "MyCamera.h"
+#pragma once
+
+class PerspectiveCamera : public MyCamera {
+
+public:
+    float xLast, yLast; //stores the last cursor position
+    bool isInitialized;//checks if the values of xLast and yLast have been initialized
+    float pitch; //the rotation of the camera around the x axis
+    float yaw; //the rotation of the camera around the y axis
+
+    //constructor for the perspective camera class
+    PerspectiveCamera(glm::vec3 position, glm::vec3 target, glm::vec3 up) : MyCamera(position, target, up) {
+        xLast = yLast = 0.0f;
+        isInitialized = false;
+        pitch = 0.0f;
+        yaw = 90.0f;
+    }
+
+    //set the value of the projection matrix in the shader
+    void setProjectionMatrix(Shader shader) override {
+        shader.useProgram();
+
+        //compute for the projection matrix
+        glm::mat4 projection_matrix = glm::perspective(
+            glm::radians(45.0f),
+            WIDTH / HEIGHT,
+            0.1f,
+            100.f
+        );
+
+        //set the value of the projection matrix in the shader
+        unsigned int projectionLoc = glGetUniformLocation(shader.shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection_matrix));
+    }
+
+    //process the mouse inputs and updates the object attributes
+    void processMouse(float xPos, float yPos) {
+        //initialize the value of xLast and yLast
+        if (!isInitialized) {
+            xLast = xPos;
+            yLast = yPos;
+            isInitialized = true;
+        }
+
+        float sensitivity = 1.0f; //controls rotation senstivity
+
+        //compute the displacement from the previous position
+        float xDiff = xPos - xLast;
+        float yDiff = yLast - yPos;
+
+        //update the last position
+        xLast = xPos;
+        yLast = yPos;
+
+        //updates the value of the pitch and yaw
+        yaw += xDiff * sensitivity;
+        pitch += yDiff * sensitivity;
+
+        //prevent lookAt flip by limiting the rotation along the x-axis to be greater than -90 deegress
+        if (pitch < -90.0f) {
+            pitch = -89.99f;
+        }
+
+        //prevent lookAt flip by limiting the rotation along the x-axis to be less than 90 deegress
+        if (pitch > 90.0f) {
+            pitch = 89.99f;
+        }
+
+        //recompute the position of the camera depending on the values of yaw and pitch
+        glm::vec3 newPosition;
+        newPosition.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch)); //the x component is influenced by the x component of the yaw and pitch
+        newPosition.y = sin(glm::radians(pitch)); //the y component is influenced by the y component of the pitch
+        newPosition.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));//the z component is influenced by the y component of the yaw and the x component of the pitch
+
+        newPosition = distance * glm::normalize(newPosition); //ensure that the distance between the camera and the target relative to the camera position is always the same
+
+        //update the camera position
+        position = newPosition;
+    }
+};

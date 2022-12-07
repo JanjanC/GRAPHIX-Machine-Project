d82af69eeedea667dce4cbadcd8c72aefc4a57f9
iDLEWINK <josh_uson@dlsu.edu.ph>
2022-12-08 04:39:56 +0800

    feat: add ortho movement
    also changed the projection matrix so it's farther away

--- Classes/Cameras/OrthoCamera.h
+++ Classes/Cameras/OrthoCamera.h
@@ -12,10 +12,36 @@ public:
         shader.useProgram();
 
         //compute for the projection matrix
-        projectionMatrix = glm::ortho(-WIDTH / 100, WIDTH / 100, -HEIGHT / 100, HEIGHT / 100, -100.0f, 100.0f);
+        projectionMatrix = glm::ortho(-WIDTH / 50, WIDTH / 50, -HEIGHT / 50, HEIGHT / 50, -100.0f, 100.0f);
 
         //set the value of the projection matrix in the shader
         unsigned int projectionLoc = glGetUniformLocation(shader.shaderProgram, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+    }
+
+    //process keyboard inputs and update the object attributes
+    void processKeyboard(int key) {
+        float sensitivity = 1.0f;
+
+        /*Traverse Forward*/
+        if (key == GLFW_KEY_W) {           
+            position.z -= sensitivity;
+            target.z -= sensitivity;
+        }
+        /*Traverse Backward*/
+        if (key == GLFW_KEY_S) {
+            position.z += sensitivity;
+            target.z += sensitivity;
+        }
+        /*Rotate to left*/
+        if (key == GLFW_KEY_A) {
+            position.x -= sensitivity;
+            target.x -= sensitivity;
+        }
+        /*Rotate to right*/
+        if (key == GLFW_KEY_D) {
+            position.x += sensitivity;
+            target.x += sensitivity;
+        }
     }
 };
